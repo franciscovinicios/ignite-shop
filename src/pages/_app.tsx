03f8c71ebee27dbaf11e0cdd4aec58b20fadb4a6
Francisco Vinicios <francisco.viniciosti@gmail.com>
2023-12-07 00:12:11 +0000

    Adding features: button cart header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from "next/app";
 import logoImg from "@/assets/logo.svg";
 import { Container, Header } from "@/styles/pages/app";
 import Image from "next/image";
+import { Handbag } from "@phosphor-icons/react";
 
 globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
@@ -11,6 +12,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
       <Header>
         <Image src={logoImg.src} alt="" width={130} height={52} />
+        <button>
+          <Handbag weight="bold" size={24} color="#8D8D99" />
+          <span>0</span>
+        </button>
       </Header>
       <Component {...pageProps} />
     </Container>

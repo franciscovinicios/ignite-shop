88f03e967013686fb8ae358748971dd5690c4c78
Francisco Vinicios <francisco.viniciosti@gmail.com>
2023-12-10 21:39:39 +0000

    Adding features: Cart Component - cart summary

--- src/components/Cart/cart.ts
+++ src/components/Cart/cart.ts
@@ -2,37 +2,61 @@ import { styled } from "@/styles";
 import * as Dialog from "@radix-ui/react-dialog";
 
 export const Overlay = styled(Dialog.Overlay, {
+  width: "100vw",
+  height: "100vh",
   position: "fixed",
   inset: 0,
 });
 export const Content = styled(Dialog.Content, {
   backgroundColor: "$gray800",
   borderRadius: 6,
+  padding: "48px",
 
   position: "fixed",
   top: "0",
   right: "0",
-  width: "100vw",
-  maxWidth: "30rem",
+  width: "30rem",
   height: "100vh",
-  padding: 25,
 });
 export const Title = styled(Dialog.Title, {
   marginTop: "4.5rem",
-  marginBottom: "2rem",
   fontSize: "1.25rem",
   fontWeight: "700",
   color: "$gray100",
 });
 
+export const CloseButton = styled(Dialog.Close, {
+  position: "absolute",
+  top: "1.5rem",
+  right: "1.5rem",
+
+  backgroundColor: "transparent",
+  border: "none",
+  cursor: "pointer",
+
+  svg: {
+    color: "#8D8D99",
+  },
+});
 export const CartSummary = styled("div", {
+  marginTop: "2rem",
   display: "flex",
+  flex: 1,
   flexDirection: "column",
+  justifyContent: "space-between",
+  height: "100%",
   gap: "1.5rem",
 });
 
+export const ItemsContainer = styled("div", {
+  display: "flex",
+  flex: 1,
+  flexDirection: "column",
+  gap: "1.5rem",
+});
 export const ItemCart = styled("div", {
   display: "flex",
+
   gap: "1.25rem",
 
   height: "5.875rem",
@@ -73,5 +97,39 @@ export const DetailsItem = styled("div", {
     cursor: "pointer",
 
     fontWeight: 700,
+  },
+});
+
+export const ResumeCartDescription = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  gap: ".1875rem",
+
+  div: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    "span.light-bold": {
+      fontSize: "1.125rem",
+      fontWeight: 700,
+    },
+
+    "span.strong-bold": {
+      fontSize: "1.5rem",
+      fontWeight: 700,
+    },
+  },
+  button: {
+    width: "100%",
+    borderRadius: "8px",
+    marginTop: "3.4375rem",
+    marginBottom: "7rem",
+    padding: "20px 2rem",
+
+    border: "none",
+    backgroundColor: "$green500",
+
+    fontSize: "1.125rem",
   },
 });
--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,16 +1,20 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import {
   CartSummary,
+  CloseButton,
   Content,
   DetailsItem,
   ImageContainer,
   ItemCart,
+  ItemsContainer,
   Overlay,
+  ResumeCartDescription,
   Title,
 } from "./cart";
 import Image from "next/image";
 
 import shirt from "@/assets/shirt.svg";
+import { X } from "@phosphor-icons/react";
 
 export function Cart() {
   return (
@@ -18,27 +22,55 @@ export function Cart() {
       <Overlay />
       <Content>
         <Title>Sacola de compras</Title>
+        <CloseButton>
+          <X size={28} />
+        </CloseButton>
         <CartSummary>
-          <ItemCart>
-            <ImageContainer className="image">
-              <Image src={shirt} alt="" />
-            </ImageContainer>
-            <DetailsItem>
-              <span>Camiseta Beyond the Limits</span>
-              <span className="bold">R$ 79,90</span>
-              <button>Remover</button>
-            </DetailsItem>
-          </ItemCart>
-          <ItemCart>
-            <ImageContainer className="image">
-              <Image src={shirt} alt="" />
-            </ImageContainer>
-            <DetailsItem>
-              <span>Camiseta Beyond the Limits</span>
-              <span className="bold">R$ 79,90</span>
-              <button>Remover</button>
-            </DetailsItem>
-          </ItemCart>
+          <ItemsContainer>
+            <ItemCart>
+              <ImageContainer className="image">
+                <Image src={shirt} alt="" />
+              </ImageContainer>
+              <DetailsItem>
+                <span>Camiseta Beyond the Limits</span>
+                <span className="bold">R$ 79,90</span>
+                <button>Remover</button>
+              </DetailsItem>
+            </ItemCart>
+            <ItemCart>
+              <ImageContainer className="image">
+                <Image src={shirt} alt="" />
+              </ImageContainer>
+              <DetailsItem>
+                <span>Camiseta Beyond the Limits</span>
+                <span className="bold">R$ 79,90</span>
+                <button>Remover</button>
+              </DetailsItem>
+            </ItemCart>
+            <ItemCart>
+              <ImageContainer className="image">
+                <Image src={shirt} alt="" />
+              </ImageContainer>
+              <DetailsItem>
+                <span>Camiseta Beyond the Limits</span>
+                <span className="bold">R$ 79,90</span>
+                <button>Remover</button>
+              </DetailsItem>
+            </ItemCart>
+          </ItemsContainer>
+
+          <ResumeCartDescription>
+            <div>
+              <span>Quantidade</span>
+              <span>3 itens</span>
+            </div>
+            <div>
+              <span className="light-bold">Valor total</span>
+              <span className="strong-bold">R$ 270,00</span>
+            </div>
+
+            <button>Finalizar Compra</button>
+          </ResumeCartDescription>
         </CartSummary>
       </Content>
     </Dialog.Portal>
